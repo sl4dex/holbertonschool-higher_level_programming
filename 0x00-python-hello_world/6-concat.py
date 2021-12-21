55ee3db6f49dbf1328631e6c9685458b3d07c903
salvador <3940@holbertonschool.com>
2021-12-20 18:32:43 -0800

    created workspace and added ex 0 to 9

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+str1 = "Holbeton " + "School"
+print("Welcome to {}!".format(str1))
---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+s1 = word[:3]
+s2 = word[7:]
+s3 = word[1:-1]
+print("First 3 letters: {}".format(s1))
+print("Last 2 letters: {}".format(s2))
+print("Middle word: {}".format(s3))
