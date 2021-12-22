aae0f69b194651d1fe60d56db94ff1afd0361d1d
salvador <3940@holbertonschool.com>
2021-12-22 13:08:57 -0800

    fixing ex 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 s1 = word[:3]
-s2 = word[7:]
+s2 = word[-2:]
 s3 = word[1:-1]
 print("First 3 letters: {}".format(s1))
 print("Last 2 letters: {}".format(s2))
