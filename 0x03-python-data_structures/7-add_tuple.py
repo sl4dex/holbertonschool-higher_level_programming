e522e505a9f5190ac3fa0bd0a0ea391eebdf0d38
salvador <3940@holbertonschool.com>
2022-01-03 10:39:33 -0800

    fixed case in ex 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -13,7 +13,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return (tuple_b[0], tuple_b[1])
 
     elif len(tuple_a) == 1 and len(tuple_b) == 1:
-        return (tuple_a[0], tuple_b[0])
+        return (tuple_a[0] + tuple_b[0], 0)
     elif len(tuple_a) == 2 and len(tuple_b) == 1:
         return (tuple_a[0] + tuple_b[0], tuple_a[1])
     elif len(tuple_a) == 1 and len(tuple_b) == 2:
--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 add_tuple = __import__('7-add_tuple').add_tuple
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
+tuple_a = (2,3)
+tuple_b = (2, 3)
+new_tuple = add_tuple(tuple_a, tuple_b)#4, 6
 print(new_tuple)
 
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+print(add_tuple(tuple_a, ()))#2, 3
+print(add_tuple(tuple_a, (1, )))#3, 3
+print(add_tuple((1, ), tuple_b))#3, 3
+print(add_tuple((1, ), (2, )))#3, 0
+print(add_tuple((), tuple_b))#2, 3
