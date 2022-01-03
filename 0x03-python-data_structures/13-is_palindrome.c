ab1786e81e8564a0961e67d66d48971dafd58da6
salvador <3940@holbertonschool.com>
2022-01-03 13:39:27 -0800

    fixing ex 3

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * is_palindrome - checks if linkedlist is palindrome
+ * @head: head of linkedlist
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	return (1);
+}
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in range(1, len(my_list) + 1):
-        print("{}".format(my_list[-i]))
+    my_list.reverse()
+    for x in my_list:
+        print("{:d}".format(x))
