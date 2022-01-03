bd8db695940c8e7df40400eb7a4adabe75358e41
salvador <3940@holbertonschool.com>
2022-01-03 07:33:06 -0800

    created workspace, added ex 0 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        print()
+    else:
+    for row in matrix:
+        for elem in row:
+            print("{}".format(elem))
+		    if elem 
+        print()
+
