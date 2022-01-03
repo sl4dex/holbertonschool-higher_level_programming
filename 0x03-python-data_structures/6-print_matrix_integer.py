abb513bf933153734481ddc0a6ba9a05bfa70254
salvador <3940@holbertonschool.com>
2022-01-03 08:33:28 -0800

    different way of solving 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,3 @@
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        print()
-    else:
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end='')
-                if j != len(matrix[i]) - 1:
-                    print("", end=' ')
-            print()
+    for i in matrix:
+        print(*i)
