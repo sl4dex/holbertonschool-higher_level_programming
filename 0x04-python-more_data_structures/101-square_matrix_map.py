6feca71abbc15cf451fe90b12187849c7ad0c27a
salvador <3940@holbertonschool.com>
2022-01-04 18:04:02 -0800

    fix ex101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(list(map(lambda e: e**2, row)) for row in matrix.copy())
+    return list(map(lambda row: list(map(lambda elem: elem**2, row)), matrix))
