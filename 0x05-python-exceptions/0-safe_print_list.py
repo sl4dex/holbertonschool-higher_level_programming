ddfbac1277ce01fab5776902564949f7ca86d655
sl4 <s4lv4dord@gmail.com>
2022-01-17 08:45:25 -0800

    fixing ex0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x == 0:
-        return 0
-    try:
-        for pos in range(0, x):
+    printed = 0
+    for pos in range(0, x):
+        try:
             print(my_list[pos], end="")
-        return pos + 1
-    except IndexError:
-        return pos
-    finally:
-        print()
+        except IndexError:
+            break
+        else:
+            printed += 1
+    print()
+    return printed
