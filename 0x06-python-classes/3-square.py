d9fd3c5874e16e5af8f5ecce87d17ec4c065053d
sl4 <s4lv4dord@gmail.com>
2022-01-19 15:46:27 -0800

    added ex3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Not importing any module"""
+
+
+class Square:
+    """Initiates a Square object"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """returns area of the square"""
+        return(self.__size ** 2)
