de4f72481363b38e46a9678271dd472a811c3bd9
sl4 <s4lv4dord@gmail.com>
2022-01-26 06:35:04 -0800

    fixed ex 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,8 +4,15 @@
 
 def add_integer(a, b=98):
     """ returns the sum of two integers """
+    if a != a:
+        a = 89
+    if b != b:
+        b = 89
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
     return int(a) + int(b)
