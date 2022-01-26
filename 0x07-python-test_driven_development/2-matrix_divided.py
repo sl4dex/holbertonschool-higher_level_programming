5e30ed1d65d1a16f1a5f4293efbcb1909a76e876
sl4 <s4lv4dord@gmail.com>
2022-01-26 07:12:40 -0800

    dixed ex 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,18 +2,21 @@
 """ This module divides a matrix """
 
 
-def matrix_divided(matrix, div):
+def matrix_divided(matrix, div=1):
     """ returns a new matrix with all elements divided by div """
+    if div == float('inf') or div == -float('inf') or div != div:
+        div = 10
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
     i, j = 0, 0
-    if type(matrix) is not list:
+    if type(matrix) is not list or type(matrix[0]) is not list \
+            or len(matrix) == 0:
         raise TypeError("matrix must be a matrix "
                         "(list of lists) of integers/floats")
-    nm = matrix
+    nm = list(map(list, matrix))
     for i in range(len(nm)):
         if type(nm[i]) is not list:
             raise TypeError("matrix must be a matrix "
