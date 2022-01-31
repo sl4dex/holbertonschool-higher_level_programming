747cb016a36988c28bb49fc76fa05e58a7df3fba
sl4 <s4lv4dord@gmail.com>
2022-01-31 14:24:27 -0800

    fixing exs 8 9 10

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Rectangle Module """
-
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """ Rectangle subclass """
     def __init__(self, width, height):
