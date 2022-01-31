323ea56fdbd95666cef7b520c9be33f2ed9c4147
sl4 <s4lv4dord@gmail.com>
2022-01-31 14:19:28 -0800

    fixing exs 8 9 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,7 +6,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ Square subclass """
     def __init__(self, size):
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,7 +6,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """ Rectangle subclass """
     def __init__(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
