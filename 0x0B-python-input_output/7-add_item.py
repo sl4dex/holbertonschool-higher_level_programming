f857686068b315e8b6aa8899e9d38a4566ca3f69
sl4 <s4lv4dord@gmail.com>
2022-02-01 15:37:52 -0800

    ex 5 was wrong, ex 7 dragged the error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ Add args to JSON file Module """
 from sys import argv
-import json
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
 try:
     lst = load_from_json_file("add_item.json")
     save_to_json_file(lst + argv[1:], "add_item.json")
