43aab9c9b48403d1c0073e7fa3e7ad32bb7cc8ed
sl4 <s4lv4dord@gmail.com>
2022-02-01 15:25:20 -0800

    fixing ex 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """ Add args to JSON file Module """
-from os.path import exists
-from os import stat
-import sys
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-lst = []
-if exists("add_item.json") and stat("add_item.json").st_size != 0:
+try:
     lst = load_from_json_file("add_item.json")
-for arg in sys.argv[1:]:
-    lst.append(arg)
-save_to_json_file(lst, "add_item.json")
+    save_to_json_file(lst + argv[1:], "add_item.json")
+except Exception:
+    save_to_json_file(argv[1:], "add_item.json")
