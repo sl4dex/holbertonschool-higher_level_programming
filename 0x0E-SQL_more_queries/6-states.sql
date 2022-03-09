0ff32478dd7e39d6de139a2f3347b48fd1badfd1
sl4 <s4lv4dord@gmail.com>
2022-03-09 08:53:16 -0800

    added ex 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- 6. States table
+-- creates the database hbtn_0d_usa and the table states (in db hbtn_0d_usa)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
+)
