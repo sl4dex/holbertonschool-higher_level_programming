89d0b3138ee123fb243bf9b9c465c248d8384fae
sl4 <s4lv4dord@gmail.com>
2022-03-09 12:10:10 -0800

    added ex 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- 15. Only Comedy
+-- lists all shows  of the genre Comedy
+SELECT tv_shows.title
+FROM tv_show_genres
+	INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title
