debd5904a599ce135a06f2e68be5e97a464f107f
sl4 <s4lv4dord@gmail.com>
2022-03-09 12:46:45 -0800

    added ex 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- 16. List shows and genres
 -- lists all shows and all their genres (even if null)
 SELECT tv_shows.title, tv_genres.name
-FROM tv_show_genres
-	RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-	RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+FROM tv_shows
+	LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+	LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title, tv_genres.name
