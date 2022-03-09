e4e8a2a25305d01f792330af59c23ad61d7b2ef5
sl4 <s4lv4dord@gmail.com>
2022-03-09 11:44:53 -0800

    added ex 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- 13. Number of shows by genre
+-- lists all shows in db hbtn_0d_tvshows and counts its genres
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.show_id
+GROUP BY genre
+ORDER BY number_of_shows DESC
