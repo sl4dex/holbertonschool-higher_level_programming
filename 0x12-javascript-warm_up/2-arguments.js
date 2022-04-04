2ad70c391e6bff62327c445c8b110a78b451b22e
sl4 <3940@holbertonschool.com>
2022-04-04 14:00:33 -0700

    fxing ex 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ const a = process.argv;
 if (!a[2]) {
   console.log('No argument');
 } else if (a[2] && !a[3]) {
-  console.log('Argument passed');
+  console.log('Argument found');
 } else if (a[2] && a[3]) {
-  console.log('Arguments passed');
+  console.log('Arguments found');
 }
