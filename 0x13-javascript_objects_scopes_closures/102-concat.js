39468316ba2e25368d5ca40d28cbccd7650a8c9b
sl4 <3940@holbertonschool.com>
2022-04-05 15:36:13 -0700

    changed file concat order

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const fileA = './' + process.argv[2];
 const fileB = './' + process.argv[3];
 const fileC = './' + process.argv[4];
-const dataA = fs.readFileSync(fileB, 'utf-8');
-const dataB = fs.readFileSync(fileA, 'utf-8');
+const dataA = fs.readFileSync(fileA, 'utf-8');
+const dataB = fs.readFileSync(fileB, 'utf-8');
 fs.writeFile(fileC, dataA + dataB, err => {
   if (err) {
     console.error(err);
