f61edd3eee959e6515e8b6efa06a42fd4f077659
sl4 <3940@holbertonschool.com>
2022-04-05 09:13:21 -0700

    added ex 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    let h, w, row;
+    for (h = this.height; h >= 1; h--) {
+      for (w = this.width, row = ''; w >= 1; w--) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Rectangle;
