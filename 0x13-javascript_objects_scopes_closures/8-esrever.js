de58dbf86037d71945d0585edff69f3849c259f6
sl4 <3940@holbertonschool.com>
2022-04-05 12:53:53 -0700

    whoops, cant use bulitin reverse on ex 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.reverse();
+  let i;
+  const rlist = [];
+  for (i = list.length - 1; i > -1; i--) {
+    rlist.push(list[i]);
+  }
+  return rlist;
 };
