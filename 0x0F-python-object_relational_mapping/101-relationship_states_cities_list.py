1c9f1f66ca998979001481c2c70a130a1f5043a7
sl4 <s4lv4dord@gmail.com>
2022-04-10 16:56:01 -0700

    changing names

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     engine = create_engine(
         f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
     session = Session(bind=engine)
-    rows = session.query(State).join(City).order_by(State.id, City.id)
-    for stat in rows:
-        print(f"{stat.id}: {stat.name}")
-        for cit in stat.cities:
-            print(f"\t{cit.id}: {cit.name}")
+    states = session.query(State).join(City).order_by(State.id, City.id)
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"\t{city.id}: {city.name}")
