23a96357701c2ed1cdb66576fd2310b4fc22ae6e
sl4 <s4lv4dord@gmail.com>
2022-04-10 16:45:11 -0700

    added ex 101

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""update state module"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from model_city import City
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
+    session = Session(bind=engine)
+    rows = session.query(State).join(City)
+    for stat in rows:
+        print(f"{stat.id}: {stat.name}")
+        for cit in stat.cities:
+            print(f"\t{cit.id}: {cit.name}")
