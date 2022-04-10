193e630a90f3c13db8dc293e44bf668c1519e5a0
sl4 <s4lv4dord@gmail.com>
2022-04-09 18:08:24 -0700

    added ex 14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""SQLalchemy city module"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """City class"""
+    __tablename__ = 'cities'
+    # pk=True sets autoincrement=True and nullable=False
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,6 +2,7 @@
 """SQLalchemy state module"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
@@ -13,3 +14,4 @@ class State(Base):
     # pk=True sets autoincrement=True and nullable=False
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City")
