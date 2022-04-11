8e766e4bd24c0d1b8a0411b4b81e58e1c4a3228d
sl4 <s4lv4dord@gmail.com>
2022-04-10 17:00:13 -0700

    removed join

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     engine = create_engine(
         f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
     session = Session(bind=engine)
-    states = session.query(State).join(City).order_by(State.id, City.id)
+    states = session.query(State).order_by(State.id, City.id)
     for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
