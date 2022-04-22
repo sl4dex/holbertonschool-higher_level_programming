49703a9093d1d5aeabfd2c9bb7583c79181aa8bf
sl4 <3940@holbertonschool.com>
2022-04-22 21:38:02 +0000

    added ex 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""get status module"""
+import urllib.request
+
+
+req = urllib.request.Request('https://intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    page = response.read()
+t = type(page)
+utf = page.decode("utf-8")
+print("Body response:\
+    \n\t- type: {} \
+    \n\t- content: {}\
+    \n\t- utf8 content: {}".format(t, page, utf))
