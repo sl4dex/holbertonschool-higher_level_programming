91c1a230e89f965114a90d38e3d5753c26377cb2
sl4 <3940@gmail.com>
2022-04-24 18:34:39 +0000

    format

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """get header module"""
 import urllib.request
-from sys import argv
+import sys
 
 
-req = urllib.request.Request(argv[1])
+req = urllib.request.Request(sys.argv[1])
 with urllib.request.urlopen(req) as response:
     page = response.getheader("X-Request-Id")
 print(page)
