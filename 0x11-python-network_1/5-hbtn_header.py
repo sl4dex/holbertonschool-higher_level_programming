d08edf7cddfbc5aa2f41b5562c5af12a89e31eca
sl4 <3940@gmail.com>
2022-04-24 21:41:15 +0000

    fixing ex 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,6 +3,10 @@
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
     req = requests.get(argv[1])
-    print(req.headers['X-Request-Id'])
+    try:
+        print(req.headers['X-Request-Id'])
+    except KeyError:
+        pass
