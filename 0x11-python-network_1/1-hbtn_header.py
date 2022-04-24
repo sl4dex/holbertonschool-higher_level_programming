b7d6411c14082411269b7152a789cf7807bec622
sl4 <3940@gmail.com>
2022-04-24 18:45:13 +0000

    not executed when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,7 +4,8 @@ import urllib.request
 import sys
 
 
-req = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(req) as response:
-    page = response.getheader("X-Request-Id")
-print(page)
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as response:
+        page = response.getheader("X-Request-Id")
+    print(page)
